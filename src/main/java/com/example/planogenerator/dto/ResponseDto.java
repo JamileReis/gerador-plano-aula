d14526689acc574b24c7726cb1316535b23e19f5
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2025-10-13 12:29:26 -0300

    commit

---
+++ src/main/java/com/example/planogenerator/dto/ResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.planogenerator.dto;
+public class ResponseDto {
+  public String introducao;
+  public String objetivoBNCC;
+  public String passoAPasso;
+  public String rubrica;
+  public String tema;
+  public String nivel;
+  public String idade;
+}
