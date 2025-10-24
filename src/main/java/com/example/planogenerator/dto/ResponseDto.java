2be8ed59e78fba061d7d70b707bc0dfb95c9c041
JamileReis <mile_matos@gmail.com>
2025-10-24 19:51:24 -0300

    feat

---
+++ src/main/java/com/example/planogenerator/dto/ResponseDto.java
@@ -0,0 +1,62 @@
+package com.example.planogenerator.dto;
+
+
+import java.util.List;
+import java.util.Map;
+
+public class ResponseDto {
+    private String introducao;
+    private String objetivoBNCC;
+    private List<String> passoAPasso;
+    private List<Map<String, String>> rubrica;
+    private String tema;
+    private String nivel;
+    private String idade;
+    private Integer duracao;
+
+    public ResponseDto() {}
+
+    public ResponseDto(String introducao, String objetivoBNCC, List<String> passoAPasso, List<Map<String, String>> rubrica, String tema, String nivel, String idade, Integer duracao) {
+        this.introducao = introducao;
+        this.objetivoBNCC = objetivoBNCC;
+        this.passoAPasso = passoAPasso;
+        this.rubrica = rubrica;
+        this.tema = tema;
+        this.nivel = nivel;
+        this.idade = idade;
+        this.duracao = duracao;
+    }
+
+    public String getIntroducao() {
+        return introducao;
+    }
+
+    public String getObjetivoBNCC() {
+        return objetivoBNCC;
+    }
+
+    public List<String> getPassoAPasso() {
+        return passoAPasso;
+    }
+
+    public List<Map<String, String>> getRubrica() {
+        return rubrica;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public Integer getDuracao() {
+        return duracao;
+    }
+}
+
